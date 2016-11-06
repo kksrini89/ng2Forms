68acfc8a5cca772896fac1eea437acabb057c1d3
Srinivasan K K <Srinivasan K K>
2016-11-06 17:42:43 +0530

    ng2Forms-Template Driven is implemented

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -8,11 +8,18 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class DataDrivenComponent implements OnInit {
   myForm: FormGroup;
+  genders: [
+    'male',
+    'female'
+  ];
   constructor() {
     this.myForm = new FormGroup({
-      "username": new FormControl('Srini', Validators.required),
-      "email": new FormControl('srini@yahoo.com', Validators.required),
-      "password": new FormControl('test', Validators.required)
+      "userData": new FormGroup({
+        "username": new FormControl('Srini', Validators.required),
+        "email": new FormControl('srini@yahoo.com', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")])
+      }),
+      "password": new FormControl('test', Validators.required),
+      "gender": new FormControl('male')
     });
   }
 
