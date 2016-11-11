2c9fd7cb96baeb21f935ba6b533a43fc4bfb586d
Srinivasan K K <Srinivasan K K>
2016-11-11 20:10:18 +0530

    Data driven template is changed

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-data-driven',
@@ -12,23 +13,59 @@ export class DataDrivenComponent implements OnInit {
     'male',
     'female'
   ];
-  constructor() {
-    this.myForm = new FormGroup({
-      "userData": new FormGroup({
-        "username": new FormControl('Srini', Validators.required),
-        "email": new FormControl('srini@yahoo.com', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")])
+  constructor(private formBuilder: FormBuilder) {
+    // this.myForm = new FormGroup({
+    //   "userData": new FormGroup({
+    //     "username": new FormControl('Srini', [Validators.required, this.customValidator]),
+    //     "email": new FormControl('srini@yahoo.com', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")])
+    //   }),
+    //   "password": new FormControl('test', Validators.required),
+    //   "gender": new FormControl('male'),
+    //   "hobbies": new FormArray([new FormControl('Cooking', Validators.required, this.asyncCustomValidator)])
+    // });
+
+    this.myForm = formBuilder.group({
+      "userData": formBuilder.group({
+        "username": ['Srini', [Validators.required, this.customValidator], this.asyncCustomValidator],
+        "email": ['srini@yahoo.com', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]]
       }),
-      "password": new FormControl('test', Validators.required),
-      "gender": new FormControl('male'),
-      "hobbies": new FormArray([new FormControl('Cooking', Validators.required)])
-    });
+      "password": ['test', Validators.required],
+      "gender": ['male'],
+      "hobbies": formBuilder.array([
+        ['Cooking', Validators.required, this.asyncCustomValidator]
+      ])
+    })
+    // this.myForm.statusChanges.subscribe((data) => {
+    //   console.log(data)
+    // });
+    this.myForm.valueChanges.subscribe((data) => console.log(data));
   }
 
   ngOnInit() {
   }
 
+  customValidator(control: FormControl): { [s: string]: boolean } {
+    if (control.value === 'Custom') {
+      return { custom: true };
+    }
+    return null;
+  }
+
+  asyncCustomValidator(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Custom') {
+          resolve({ custom: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+
   onAddHobby() {
-    (<FormArray>this.myForm.controls['hobbies']).push(new FormControl('', Validators.required));
+    (<FormArray>this.myForm.controls['hobbies']).push(new FormControl('', Validators.required, this.asyncCustomValidator));
   }
 
   onSubmit() {
